081e25449d369cc80bec27b241f28084f7e9100d
iamdivypatel <90637762+iamdivypatel@users.noreply.github.com>
2024-09-26 06:45:05 +0000

    variables &  datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 122334
+let accountEmail = "jay@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ahmedabad"
+let accountState;
+
+// accountId = 2 // not allowd
+
+
+accountEmail = "jv.vj.com"
+accountPassword = "2122"
+accountCity = "kadi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]
+)
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict" // treat all js coad new version
+
+// alert (3 + 3); //we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability shoud be high
+
+console.log("Jay Patel");
+
+let name = "jay"
+let age = 19
+let isLoggedIn = false
+let state;
+
+
+// number => 2 power 53
+// bigint
+// string ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbole => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object 
+
