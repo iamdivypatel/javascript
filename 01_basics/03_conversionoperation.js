6d0daa80b99ede03facd92145878df0d43373c4b
iamdivypatel <90637762+iamdivypatel@users.noreply.github.com>
2024-09-27 05:51:11 +0000

    conversionoperation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,28 @@
+let score = "jay"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let ValueInNumber = Number(score)
+//console.log(typeof ValueInNumber);
+//console.log(ValueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// True => 1 , False => 0 (boolean)
+
+let isLoggedIn = "jay"
+
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoogedIn);
+
+// 1 => true , 0 => false
+// "" => false
+// "Jay" => true
+
+let someNumber = 44
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
