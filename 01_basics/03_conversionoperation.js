aa0dcd0652b85fbf115fcc9b4c236b0f068bbae2
iamdivypatel <90637762+iamdivypatel@users.noreply.github.com>
2024-09-28 05:42:00 +0000

    conversionoperation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -14,8 +14,8 @@ let ValueInNumber = Number(score)
 
 let isLoggedIn = "jay"
 
-let booleanIsLoogedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoogedIn);
+// let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn);
 
 // 1 => true , 0 => false
 // "" => false
@@ -24,5 +24,47 @@ console.log(booleanIsLoogedIn);
 let someNumber = 44
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// --------------------operation--------------------------
+
+let value = 10
+let negValue = -10
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello "
+let str2 = "Jay Patel"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("2" + 4);
+// console.log(2 + "4");
+// console.log("2" + 2 + 4);
+// console.log(2 + 2 + "4");
+
+// console.log("2" + (2 + 4));
+
+// console.log(2 + 9 * 4 / 5 % 2);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+// num1 = num2 = num3 = 3 + 4
+// console.log(num1);
+ 
+let gameCounter = 111
+gameCounter++;
+console.log(gameCounter);
+
